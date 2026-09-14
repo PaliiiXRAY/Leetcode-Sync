86587efde43ba0c30c0583126ce770db6ca1cb00
Pallabi Pandi <pallabitheprincess74@gmail.com>
2026-09-13 17:24:10 -1200

    Time: 25 ms (57.31%), Space: 24.1 MB (14.5%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    bool backtracking(vector<int>& nums, int target, int index, vector<int>& subsetSums) {
-        if (index == nums.size()) {
+    bool solve(int mask, vector<int>& arr, int target, int k, int bucket_no,
+               int bucket_sum, vector<int>& dp) {
+        if (bucket_no == k) {
             return true;
         }
-        for (int i = 0; i < subsetSums.size(); i++) {
-            if (subsetSums[i] + nums[index] <= target) {
-                subsetSums[i] += nums[index];
-                
-                if (backtracking(nums, target, index + 1, subsetSums)) {
-                    return true;
+        if (bucket_sum == target) {
+            return solve(mask, arr, target, k, bucket_no + 1, 0, dp);
+        }
+        if (dp[mask] != -1) {
+            return dp[mask];
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            if ((mask & (1 << i)) == 0) {
+                if (bucket_sum + arr[i] <= target) {
+                    int new_mask = mask | (1 << i);
+                    if (solve(new_mask, arr, target, k, bucket_no,
+                              bucket_sum + arr[i], dp)) {
+                        return dp[mask] = 1;
+                    }
                 }
-                subsetSums[i] -= nums[index];
-            }
-            if (subsetSums[i] == 0) {
-                break;
             }
         }
-        return false;
+        return dp[mask] = 0;
     }
 
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
+        for (int num : nums) {
+            sum += num;
         }
         if (sum % k != 0) {
             return false;
         }
         int target = sum / k;
-        sort(nums.begin(), nums.end(), greater<int>()); 
-        vector<int> subsetSums(k, 0); 
-        return backtracking(nums, target, 0, subsetSums);
+        int n = nums.size();
+        vector<int> dp(1 << n, -1);
+        return solve(0, nums, target, k, 0, 0, dp);
     }
 };
